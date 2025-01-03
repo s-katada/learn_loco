b97a1eb565d71ad1b6313ec2c7d578bea7a9a84f
ShunyaKatada <awesomemr@s-katada-private.local>
2025-01-04 00:34:47 +0900

    chore: add sass plugin

--- frontend/rsbuild.config.ts
+++ frontend/rsbuild.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from "@rsbuild/core";
 import { pluginReact } from "@rsbuild/plugin-react";
+import { pluginSass } from "@rsbuild/plugin-sass";
 
 // https://rsbuild.dev/guide/basic/configure-rsbuild
 export default defineConfig({
-  plugins: [pluginReact()],
+  plugins: [pluginReact(), pluginSass()],
   html: {
     favicon: "src/assets/favicon.ico",
-    title: "Loco SaaS Starter",
+    title: "岐阜県中津川市福岡どんぐりピアノ教室",
   },
   server: {
     proxy: {
