68d53d31b918752f0a7959b9a81d7d83d48bc775
s-katada <shunya.saitama@gmail.com>
2025-01-13 21:03:59 +0900

    feat: deploy config

--- src/bin/shuttle.rs
+++ src/bin/shuttle.rs
@@ -2,18 +2,19 @@ use loco_rs::boot::{create_app, StartMode};
 use loco_rs::environment::Environment;
 use donguri_piano::app::App;
 use migration::Migrator;
+use shuttle_axum::ShuttleAxum;
 use shuttle_runtime::DeploymentMetadata;
 
 #[shuttle_runtime::main]
 async fn main(
+  #[shuttle_shared_db::Postgres] conn_str: String,
   #[shuttle_runtime::Metadata] meta: DeploymentMetadata,
-) -> shuttle_axum::ShuttleAxum {
+) -> ShuttleAxum {
+    std::env::set_var("DATABASE_URL", conn_str);
     let environment = match meta.env {
         shuttle_runtime::Environment::Local => Environment::Development,
         shuttle_runtime::Environment::Deployment => Environment::Production,
     };
-    // environmentを出力
-    println!("environment: {:?}", environment);
     let boot_result = create_app::<App, Migrator>(StartMode::ServerOnly, &environment)
         .await
         .unwrap();
